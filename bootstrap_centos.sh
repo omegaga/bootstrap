c60b1368197a3d80f9dba450d84628f33eac911b
omegaga <jhli@fb.com>
2016-05-18 10:54:44 -0700

    Change ycm installation

--- bootstrap_centos.sh
+++ bootstrap_centos.sh
@@ -2,7 +2,7 @@
 
 PWD=$(pwd)
 echo "Installing dependencies..."
-sudo yum -y install zsh vim byobu clang-3.4.2 cmake valgrind
+sudo yum -y install zsh vim byobu clang-3.4.2 cmake valgrind fb-ycm-1.1
 
 echo "Creating build directory..."
 rm -rf build
@@ -18,23 +18,24 @@ echo "Installing z..."
 cd ~/.oh-my-zsh
 rm -rf z
 git clone https://github.com/rupa/z.git
-cd $PWD
+cd "${PWD}"
 
 # Update zshrc
-cp zshrc ~/.zshrc
-chsh /bin/zsh
+cp "${PWD}"/zshrc ~/.zshrc
 
 echo "Installing spf13-vim..."
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
-rm spf13-vim.sh
-cp vimrc.local ~/.vimrc.local
-cp vimrc.before.local ~/.vimrc.before.local
+rm "${PWD}"/spf13-vim.sh
+cp "${PWD}"/vimrc.local ~/.vimrc.local
+cp "${PWD}"/vimrc.before.local ~/.vimrc.before.local
 # Clean up vimviews to make changes effective
 rm ~/.vimviews
 
-echo "Installing YouCompleteMe..."
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-cd $PWD
+# YCM
+cd ~/.vim/bundle && rm -rf YouCompleteMe
+ln -s /usr/share/vim/vim74/bundle/YouCompleteMe .
+
+cd "${PWD}"
 
 # Configure byobu
+byobu-enable
